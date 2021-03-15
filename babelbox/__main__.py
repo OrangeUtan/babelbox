9fbcb08ce26ed8cf9c8ce8af1e3c2bf52ac1e233
Oran9eUtan <Oran9eUtan@gmail.com>
2021-03-15 16:39:23 +0100

    Added cli

---
+++ babelbox/__main__.py
@@ -0,0 +1,59 @@
+import json
+import os
+from collections import defaultdict
+from pathlib import Path
+from typing import Optional
+
+import typer
+
+import babelbox
+
+
+def combine_locales_from_files(files):
+    locales = defaultdict(dict)
+    for f in files:
+        for locale_name, translations in babelbox.load_locales_from_csv(f).items():
+            locales[locale_name].update(translations)
+
+    return locales
+
+
+def get_csv_files_in_dir(dir: Path):
+    for entry in os.walk(dir):
+        files = map(lambda f: Path(entry[0], f), entry[2])
+        csv_files = filter(lambda f: f.name.endswith(".csv"), files)
+        yield from csv_files
+
+
+def write_locale(out_dir: Path, locale: str, entries: dict, indent: Optional[str] = None):
+    with Path(out_dir, f"{locale}.json").open("w", encoding="utf-8") as f:
+        json.dump(entries, f, indent=indent, ensure_ascii=False)
+
+
+app = typer.Typer()
+
+
+@app.command()
+def main(
+    src_dir: str,
+    out_dir: Optional[str] = typer.Argument(
+        None, help="The directory in which to generate the language localization files"
+    ),
+    indent: str = typer.Option("\t", help="String used to indent json"),
+    no_indent: int = typer.Option(
+        False, help="Don't pretty print json with indentation", is_flag=True, flag_value=True
+    ),
+):
+    src_dir_path = Path(src_dir)
+    if not src_dir_path.exists():
+        raise FileNotFoundError(src_dir_path)
+    out_dir_path = Path(out_dir) if out_dir else Path(src_dir_path)
+    out_dir_path.mkdir(parents=True, exist_ok=True)
+
+    csv_files = get_csv_files_in_dir(src_dir_path)
+    for locale_name, entries in combine_locales_from_files(csv_files).items():
+        write_locale(out_dir_path, locale_name, entries, None if no_indent else indent)
+
+
+if __name__ == "__main__":
+    app()  # pragma: no cover
---
+++ tests/cli/test_main.py
@@ -0,0 +1,100 @@
+import unittest
+from pathlib import Path
+from unittest import mock
+from unittest.mock import MagicMock, call, patch
+
+import pytest
+
+from babelbox import __main__ as cli
+
+
+def mock_mkdir(*args, **kwargs):
+    pass
+
+
+def mock_open(*args, **kwargs):
+    m = unittest.mock.mock_open(*args, **kwargs)
+    m.return_value.seek = m.side_effect  # Fix for seek(0)
+    return m
+
+
+class Test_get_csv_files_in_dir:
+    def test(self):
+        expected_files = [
+            Path("tests/cli/res/a.csv"),
+            Path("tests/cli/res/b.csv"),
+            Path("tests/cli/res/subfolder/c.csv"),
+        ]
+        files = list(cli.get_csv_files_in_dir(Path("tests/cli/res")))
+        assert files == expected_files
+
+
+class Test_combine_locales_from_files:
+    def test(self):
+        expected_locales = {
+            "a": {"x": "1", "y": "10", "s": "1", "t": "10", "ä": "1", "ö": "10"},
+            "b": {"x": "2", "y": "11", "s": "2", "t": "11", "ä": "2", "ö": "11"},
+            "c": {"x": "3", "y": "12", "s": "3", "t": "12", "ä": "3", "ö": "12"},
+        }
+
+        csv_files = [
+            Path("tests/cli/res/a.csv"),
+            Path("tests/cli/res/b.csv"),
+            Path("tests/cli/res/subfolder/c.csv"),
+        ]
+        locales = cli.combine_locales_from_files(csv_files)
+
+        assert locales == expected_locales
+
+
+class Test_create_locale:
+    def test(self):
+        expected_locale_path = Path("tests/cli/res/en.json")
+        expected_data = {"x": "1", "y": "2"}
+
+        src_dir = Path("tests/cli/res")
+        locale = "en"
+        entries = {"x": "1", "y": "2"}
+        with patch("io.open", new=MagicMock()) as mockfile:
+            with patch("json.dump", new=MagicMock()) as mockdump:
+                cli.write_locale(src_dir, locale, entries)
+
+                mockfile.assert_called_once()
+                assert mockfile.call_args[0][0] == expected_locale_path
+
+                mockdump.assert_called_once()
+                assert mockdump.call_args[0][0] == expected_data
+
+
+class Test_main:
+    def test(self):
+        expected_call_args_list = [
+            call(
+                Path("tests/cli/res"),
+                "a",
+                {"x": "1", "y": "10", "s": "1", "t": "10", "ä": "1", "ö": "10"},
+                None,
+            ),
+            call(
+                Path("tests/cli/res"),
+                "b",
+                {"x": "2", "y": "11", "s": "2", "t": "11", "ä": "2", "ö": "11"},
+                None,
+            ),
+            call(
+                Path("tests/cli/res"),
+                "c",
+                {"x": "3", "y": "12", "s": "3", "t": "12", "ä": "3", "ö": "12"},
+                None,
+            ),
+        ]
+
+        with patch("babelbox.__main__.write_locale", new=MagicMock()) as mock_write_locale:
+            cli.main("tests/cli/res", None)
+
+            assert mock_write_locale.call_count == 3
+            assert mock_write_locale.call_args_list == expected_call_args_list
+
+    def test_file_not_found(self):
+        with pytest.raises(FileNotFoundError):
+            cli.main("doesnt_exist")
