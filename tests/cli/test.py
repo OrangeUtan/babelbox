74c52a2dae7e48ebc02df8c2d6b5f15d369f2758
Oran9eUtan <Oran9eUtan@gmail.com>
2021-03-19 21:23:44 +0100

    Added more test for parsing whole directories

--- tests/cli/test.py
+++ tests/cli/test.py
@@ -1,4 +1,3 @@
-import re
 from pathlib import Path
 from unittest.mock import MagicMock, patch
 
@@ -19,37 +18,58 @@ class Test_main:
     def test_no_args(self, runner: CliRunner):
         assert "Missing argument" in runner.invoke(cli.app).output
 
-    def test(self, runner):
-        with patch(
-            "babelbox.cli.write_language_files", new=MagicMock()
-        ) as mock_write_lang_files:
-            runner.invoke(cli.app, "tests/cli/res", catch_exceptions=False)
-
-            assert mock_write_lang_files.call_args_list[0][0][0] == Path("tests/cli/res")
-            assert mock_write_lang_files.call_args_list[0][0][1] == {
-                "a": {"x": "1", "y": "10", "s": "1", "t": "10"},
-                "b": {"x": "2", "y": "11", "s": "2", "t": "11"},
-                "c": {"x": "3", "y": "12", "s": "3", "t": "12"},
-            }
-
     def test_file_not_found(self, runner: CliRunner):
         result = runner.invoke(cli.app, "doesnt_exist")
         assert str(result.exception) == "2"
         assert "does not exist" in result.output
 
-    def test_prefix_filename(self, runner: CliRunner):
+    @pytest.mark.parametrize(
+        "src, expected, expected_prefixed",
+        [
+            (
+                "tests/cli/test_dirs/multiple_csv",
+                {
+                    "a": {"x": "1", "y": "10", "s": "1", "t": "10"},
+                    "b": {"x": "2", "y": "11", "s": "2", "t": "11"},
+                    "c": {"x": "3", "y": "12", "s": "3", "t": "12"},
+                },
+                {
+                    "a": {"a.x": "1", "a.y": "10", "b.s": "1", "b.t": "10"},
+                    "b": {"a.x": "2", "a.y": "11", "b.s": "2", "b.t": "11"},
+                    "c": {"a.x": "3", "a.y": "12", "b.s": "3", "b.t": "12"},
+                },
+            ),
+            (
+                "tests/cli/test_dirs/read_only_csv",
+                {
+                    "a": {"x": "1", "y": "10", "s": "1", "t": "10"},
+                    "b": {"x": "2", "y": "11", "s": "2", "t": "11"},
+                    "c": {"x": "3", "y": "12", "s": "3", "t": "12"},
+                },
+                {
+                    "a": {"a.x": "1", "a.y": "10", "b.s": "1", "b.t": "10"},
+                    "b": {"a.x": "2", "a.y": "11", "b.s": "2", "b.t": "11"},
+                    "c": {"a.x": "3", "a.y": "12", "b.s": "3", "b.t": "12"},
+                },
+            ),
+        ],
+    )
+    def test_dirs(self, src, expected, expected_prefixed, runner: CliRunner):
         with patch(
             "babelbox.cli.write_language_files", new=MagicMock()
         ) as mock_write_lang_files:
-            runner.invoke(cli.app, "tests/cli/res -n", catch_exceptions=False)
+            runner.invoke(cli.app, src, catch_exceptions=False)
+            runner.invoke(cli.app, [src, "-n"], catch_exceptions=False)
 
-            mock_write_lang_files.assert_called_once()
-            assert mock_write_lang_files.call_args_list[0][0][0] == Path("tests/cli/res")
-            assert mock_write_lang_files.call_args_list[0][0][1] == {
-                "a": {"a.x": "1", "a.y": "10", "b.s": "1", "b.t": "10"},
-                "b": {"a.x": "2", "a.y": "11", "b.s": "2", "b.t": "11"},
-                "c": {"a.x": "3", "a.y": "12", "b.s": "3", "b.t": "12"},
-            }
+            assert mock_write_lang_files.call_count == 2
+
+            # Not prefixed
+            assert mock_write_lang_files.call_args_list[0][0][0] == Path(src)
+            assert mock_write_lang_files.call_args_list[0][0][1] == expected
+
+            # Prefixed
+            assert mock_write_lang_files.call_args_list[1][0][0] == Path(src)
+            assert mock_write_lang_files.call_args_list[1][0][1] == expected_prefixed
 
 
 class Test_version_callback:
